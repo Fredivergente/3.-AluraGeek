f53dfd2e79acfd57525e892ac3860d2eea3aee68
Alfredo <alfredojfv@gmail.com>
2023-09-10 02:51:11 -0500

    AluraGeek

---
+++ services/product-service.js
@@ -0,0 +1,82 @@
+/** @format */
+
+const starwars = () =>
+	fetch("http://localhost:5500/product?category=starwars").then((response) =>
+		response.json()
+	);
+const consolas = () =>
+	fetch("http://localhost:5500/product?category=consolas").then((response) =>
+		response.json()
+	);
+const diversos = () =>
+	fetch("http://localhost:5500/product?category=diversos").then((response) =>
+		response.json()
+	);
+
+const allProducts = () =>
+	fetch("http://localhost:5500/product").then((response) => response.json());
+
+const crearNuevoProducto = (img, name, price, description, category) => {
+	return fetch("http://localhost:5500/product", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			img,
+			category,
+			name,
+			price,
+			description,
+			id: uuid.v4(),
+		}),
+	});
+};
+
+const eliminarProducto = (id) => {
+	return fetch(`http://localhost:5500/product/${id}`, {
+		method: "DELETE",
+	});
+};
+
+const detalleProducto = (id) => {
+	return fetch(`http://localhost:5500/product/${id}`).then((response) =>
+		response.json()
+	);
+};
+
+const actualizarProducto = (img, category, name, price, description, id) => {
+	return fetch(`http://localhost:5500/product/${id}`, {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ img, category, name, price, description, id }),
+	})
+		.then((response) => response)
+		.catch((err) => console.log(err));
+};
+
+const productoIndividual = (id) =>
+ 	fetch(`http://localhost:5500/product/${id}`).then((response) => 
+ 	response.json()
+);
+
+
+const relacionados = () => 
+	 fetch(`http://localhost:5500/product`).then((response) =>
+		response.json()
+	);
+
+export const productServices = {
+	starwars,
+	consolas,
+	diversos,
+	allProducts,
+	crearNuevoProducto,
+	eliminarProducto,
+	detalleProducto,
+	actualizarProducto,
+	productoIndividual,
+	relacionados,
+};
